07882056d46d8ef63bfbf17a325ce2a0f3842cde
TerranceKuo1 <sushitopia12@gmail.com>
2023-08-04 21:11:53 -0500

    remove faulty test

--- src/components/Button/button.test.tsx
+++
@@ -1,9 +0,0 @@
-
-'use strict';
-import renderer from 'react-test-renderer';
-import { Button } from '../Button/index';
-
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Button> Shopping Lists  </Button>)
-});
